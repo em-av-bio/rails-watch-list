90ba194adc9830077fd0dfae723bea1b5bf8dc6e
Emmanuel Landron <emmanuel@aventure.bio>
2022-11-18 16:29:00 +0100

    Front changes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,11 +8,8 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
-    if @bookmark.save
-      redirect_to list_path(@list)
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @bookmark.save
+    redirect_to list_path(@list)
   end
 
   def destroy
