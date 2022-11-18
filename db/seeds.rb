2549b7cfebe059dcc9eb8119156d1c4601b248c3
Emmanuel Landron <emmanuel@aventure.bio>
2022-11-18 09:16:45 +0100

    Too many changes, my bad

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Movie.destroy_all
+
+api_url = "https://tmdb.lewagon.com/movie/top_rated"
+top_rated = JSON.parse(URI.open(api_url).read)["results"]
+
+top_rated.each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}", rating: movie["vote_average"])
+end
+
+ap "ok"
